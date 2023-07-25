40f784e2630601e58b605a61e2352ecac32b1cf5
+ <+>
2023-07-25 11:32:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
 }
 
 function renderBooks(books) {
@@ -14,4 +14,12 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+
 });
+
+function fetchBooks() {
+  // Create a fetch request to the Game of Thrones API
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then((response) => response.json()) // Convert the response to JSON
+    .then((data) => renderBooks(data)); // Call renderBooks() with the JSON data as the argument
+}
